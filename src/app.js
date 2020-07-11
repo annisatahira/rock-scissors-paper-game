0c186bc92b44a40721e4675d5e225ce6eb5f9e7d
annisatahira <annisatahira999@gmail.com>
2020-07-11 15:48:17 +0700

    add package

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import RSPGameApp from "./components/RSPGameApp.jsx";
+import "normalize-css/normalize.css";
+
+ReactDOM.render(<RSPGameApp />, document.getElementById("app"));
---
+++ src/components/RSPGameApp.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+class RSPGameApp extends React.Component {
+  render() {
+    return <div>tes</div>;
+  }
+}
+
+export default RSPGameApp;
