08369c8a9665720dd89042b0dac49752efc5e4a2
annisatahira <annisatahira999@gmail.com>
2020-07-12 23:50:47 +0700

    fix style choose weapon

--- src/components/PlayerImage.jsx
+++ src/components/PlayerImage.jsx
@@ -1,8 +1,20 @@
 import React from "react";
+import scissors from "../../public/images/scissor.png";
+import paper from "../../public/images/paper.png";
+import rock from "../../public/images/rock.png";
 
 const PlayerImage = (props) => (
   <div>
-    <h1>{props.image}</h1>
+    <img
+      className="player-image"
+      src={
+        props.image === "rock"
+          ? rock
+          : props.image === "scissors"
+          ? scissors
+          : paper
+      }
+    />
   </div>
 );
 
